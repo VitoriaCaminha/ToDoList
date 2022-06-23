e6c10b50896108878c0779f61aca098e8afa1558
VitoriaCaminha <vitoria@praciano.com.br>
2022-06-23 18:41:51 -0300

    feat (platform): to do list

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PageHeader from "../template/pageHeader";
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="nós"></PageHeader>
+
+        <h2>Nossa história</h2>
+        <p>Lorem ipsum</p>
+        <h2>Missão e visão</h2>
+        <p>Lorem ipsum</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum</p>
+    </div>
+)
